f124cbed8116447f287d721e25a5ec19d39eab40
rushhour7 <rushhour.k@gmail.com>
2020-06-28 19:24:21 +0900

    Implement popup window

--- Carlet/app/src/main/java/com/choiysapple/carlet/BrowseFragment.java
+++ Carlet/app/src/main/java/com/choiysapple/carlet/BrowseFragment.java
@@ -1,10 +1,16 @@
 package com.choiysapple.carlet;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,16 +22,45 @@ import com.choiysapple.carlet.Model.SymbolDataManager;
 import java.util.ArrayList;
 
 public class BrowseFragment extends Fragment {
+    GridView gv;
+    SymbolDataManager dataManager;
+    ArrayList<Symbol> symbolArrayList;
+    View popupView;
+    PopupWindow popUp;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_browse, container, false);
+        return inflater.inflate(R.layout.fragment_browse, container, false);
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
-        SymbolDataManager dataManager = new SymbolDataManager();
-        ArrayList<Symbol> symbolArrayList = dataManager.getSymbols();
+        gv = getActivity().findViewById(R.id.mygrid);
+        gv.setAdapter(new SetImageAdapter(getContext()));
 
-        GridView gv = view.findViewById(R.id.mygrid);
-        gv.setAdapter(new SetImageAdapter(view.getContext()));
-        return view;
+        dataManager = new SymbolDataManager();
+        symbolArrayList = dataManager.getSymbols();
+
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                popupView = getLayoutInflater().inflate(R.layout.popup_window, null);
+                PopupWindow popUp = new PopupWindow(popupView, 1000, LinearLayout.LayoutParams.WRAP_CONTENT, true);
+
+                ImageView logo = popupView.findViewById(R.id.logo);
+                TextView name = popupView.findViewById(R.id.name);
+                TextView desc = popupView.findViewById(R.id.desc);
+
+                popUp.setAnimationStyle(-1); // apply animation
+                popUp.setElevation(20); // apply shadow
+                popUp.showAtLocation(popupView, Gravity.CENTER, 0, -300);
+
+                // load symbol data
+                logo.setImageResource(getResources().getIdentifier("symbol" + (position+1), "drawable", getActivity().getPackageName()));
+                name.setText(symbolArrayList.get(position).name);
+                desc.setText(symbolArrayList.get(position).description);
+            }
+        });
     }
 }
