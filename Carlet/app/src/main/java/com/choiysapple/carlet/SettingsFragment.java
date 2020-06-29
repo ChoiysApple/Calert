e27dfe4d06e5f0249dc86a72153f60ad4ef4c9d2
ChoiysApple <chcs1370@gmail.com>
2020-06-30 01:05:17 +0900

    Update SettingsFragment.java

--- Carlet/app/src/main/java/com/choiysapple/carlet/SettingsFragment.java
+++ Carlet/app/src/main/java/com/choiysapple/carlet/SettingsFragment.java
@@ -1,17 +1,72 @@
 package com.choiysapple.carlet;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 public class SettingsFragment extends Fragment {
 
+    Button btn_gps, btn_darkMode, btn_credit;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        btn_gps = view.findViewById(R.id.btn_GPS);
+        btn_darkMode = view.findViewById(R.id.btn_DarkMode);
+        btn_credit = view.findViewById(R.id.btn_Credit);
+
+        btn_gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo: set GPS authority
+            }
+        });
+
+        btn_gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+        return view;
+    }
+
+
+    public static final String LIGHT_MODE = "light";
+    public static final String DARK_MODE = "dark";
+    public static final String DEFAULT_MODE = "default";
+
+    public static void applyTheme(String themeColor) {
+        switch (themeColor) {
+            case LIGHT_MODE:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+
+            case DARK_MODE:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+
+            default:
+                // 안드로이드 10 이상
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                }
+                // 안드로이드 10 미만
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+                }
+                break;
+        }
     }
 }
